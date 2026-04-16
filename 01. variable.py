0bcb237c11f9af30e7c29bb5e9c76d85a1280cfe
parkyoungok <aoisora311@gmail.com>
2026-04-16 11:15:46 +0900

    1st python

---
+++ 01. variable.py
@@ -0,0 +1,25 @@
+
+
+"""
+type(10)
+print(type)
+
+num = 3
+print(num+5)
+
+
+print('Hello'+ 'World!')
+
+
+num = 4
+print(num)
+"""
+print( 5 < 6 )
+print( 5 > 6 )
+print( 11 <= 11 )
+print( 11 != 5 )
+print( 'Hello' > 'World!')
+result = 13==4
+print(result)
+
+
